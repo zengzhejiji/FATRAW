3ff6f7c4efab9d8b62a43dd8c4219604d519ea40
zengzhejiji <zengzhe12345@qq.com>
2026-04-07 18:20:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-
 with open("aa.post",mode='rb+')as f:
     f.seek(0)
     data = f.read()
-deata = data.replace(b'\x00',b'')
-rx =deata.decode('ascii')
-sea = rx.find("|")
-FileName = rx[:sea]
-content = rx[sea+1:]
+deata = data.rstrip(b'\x00')
+try:
+    sea = deata.split(b"|",1)
+    FileName = sea[0].decode("utf-8",errors='replace')
+    content = sea[1]
+except:
+    FileName = b''
+    content = b''
 while True:
     u_i = input(">>>")
     if u_i == 'ff':
         print(FileName,"\n",content)
+    if u_i == 'fmin':
+        print(FileName,"\n",content[:10])
     if u_i == "copy":
-        with open(FileName[:len(FileName)-4]+"copy.txt",mode='w',encoding='ascii') as f:
+        with open(FileName[:len(FileName)-4]+"copy.txt",mode='wb') as f:
             f.write(content)
     if u_i == "null":
         a = input("(Y/N)?")
@@ -24,17 +28,20 @@ while True:
         else:
             break
     if u_i == "import":
-        with open("test.txt",encoding='ascii',mode='r') as f:
+        with open("test.txt",mode='rb') as f:
             user_input = f.read()
         with open("aa.post",mode='rb+')as f:
             f.seek(0)
-            f.write(b"test.txt"+b"|"+user_input.encode('ascii'))
+            f.write(b"test.txt"+b"|"+user_input)
             f.seek(0)
             data = f.read()
-            deata = data.replace(b'\x00',b'')
-            rx =deata.decode('ascii')
-            sea = rx.find("|")
-            FileName = rx[:sea]
-            content = rx[sea+1:]
+            deata = data.rstrip(b'\x00')      
+            try:
+                sea = deata.split(b"|",1)
+                FileName =sea[0].decode("utf-8")
+                content = sea[1]
+            except:
+                FileName = b''
+                content = b''
     if u_i == "exit":
         break
