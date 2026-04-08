83163384b36dbeec9fa3a9651177284934656799
zengzhejiji <zengzhe12345@qq.com>
2026-04-08 19:46:30 +0800

    Refactor file handling and user input logic

--- main.py
+++ main.py
@@ -6,21 +6,30 @@ try:
     sea = deata.split(b"|",1)
     FileName = sea[0].decode("utf-8",errors='replace')
     content = sea[1]
+    FN_L = FileName.split(".")
 except:
     FileName = b''
     content = b''
+    FN_L = [b'',b'']
 while True:
     u_i = input(">>>")
     if u_i == 'ff':
-        print(FileName,"\n",content)
+        print(FileName,"\n",content.decode("utf-8",errors='replace'))
     if u_i == 'fmin':
+        print(FileName,"\n",content[:10].decode("utf-8",errors='replace'))
+    if u_i == 'ffb':
+        print(FileName,"\n",content)
+    if u_i == 'fminb':
         print(FileName,"\n",content[:10])
     if u_i == "copy":
-        with open(FileName[:len(FileName)-4]+"copy.txt",mode='wb') as f:
-            f.write(content)
+        try:
+            with open(FN_L[0]+"copy."+FN_L[1],mode='wb') as f:
+                f.write(content)
+        except:
+            pass
     if u_i == "null":
         a = input("(Y/N)?")
-        if a == "Y":
+        if a == "Y": 
             with open("aa.post",mode='wb')as f:
                 f.write(b"\x00"*1024) 
             FileName = ''
@@ -28,20 +37,25 @@ while True:
         else:
             break
     if u_i == "import":
-        with open("test.txt",mode='rb') as f:
-            user_input = f.read()
-        with open("aa.post",mode='rb+')as f:
-            f.seek(0)
-            f.write(b"test.txt"+b"|"+user_input)
-            f.seek(0)
-            data = f.read()
-            deata = data.rstrip(b'\x00')      
-            try:
-                sea = deata.split(b"|",1)
-                FileName =sea[0].decode("utf-8")
-                content = sea[1]
-            except:
-                FileName = b''
-                content = b''
+        import_file = input("import>>>")
+        try:
+            with open(import_file,mode='rb') as f:
+                user_input = f.read()
+            with open("aa.post",mode='rb+')as f:
+                f.seek(0)
+                f.write(import_file.encode("utf-8")+b"|"+user_input)
+                f.truncate()
+                f.seek(0)
+                data = f.read()
+                deata = data.rstrip(b'\x00')
+                try:
+                    sea = deata.split(b"|",1)
+                    FileName =sea[0].decode("utf-8")
+                    content = sea[1]
+                except:
+                    FileName = b''
+                    content = b''
+        except:
+            pass
     if u_i == "exit":
         break
